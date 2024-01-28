3c291cf8775abd08b46a8062d0b73ff8720058c7
Netanelbruch <59892451+Netanelbruch@users.noreply.github.com>
2024-01-28 14:50:21 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,94 +1,73 @@
 #include "NumClass.h"
-#include <stdio.h>
 
 
 
-
-int digits(int num)
-{
-    int temp = 0;
-   
-   while (num != 0) {
-      temp++;
-      num = num/10;
-   }
-    return temp;
-}
-
-
-int subtractingNumber(int num ,int temp)
-{
-    
-if (num == 0)
-        return temp;
- 
-    
-    temp = (temp * 10) + (num % 10);
- 
-    return subtractingNumber(num / 10, temp);
-} 
-
-
-
-int power(int num , int res)
-{
-    int sum = 1;
-    for (int i = 0; i < res; i++)
-    {
-        sum = sum*num;
+int ilog10(int n) {
+    int count = 0;
+    while (n > 0) {
+        n /= 10;
+        count++;
     }
-   
-    return sum;
+    return count;
 }
 
-int testArmstrongNumber(int num ,int r,int sum)
-{   
-    
-    if (num == 0)
-    {
-        return sum;
+int ipow(int base, int exponent) {
+    int result = 1;
+    for (int i = 0; i < exponent; i++) {
+        result *= base;
     }
-    sum = sum + power(num%10,r);
-    
-    return testArmstrongNumber(num/10 ,r,sum);
+    return result;
+}
+int isPalindromeRec(int arr[], int start, int end) {
+    if (start < end) {
+        if (arr[start] == arr[end]) {
+            return isPalindromeRec(arr, start + 1, end - 1);
+        }
+        return 0;
+    }
+    return 1;
 }
 
-
-int isArmstrong(int num)
-{
-    int power1 = digits(num);
-    int sum = 0;
-    int temp = testArmstrongNumber(num,power1,sum);
-
-    if(temp == num)
-    {
-      
+int isPalindrome(int i) {
+    int size = ilog10(i);
+    int arr[size + 1];
+    int count = 0;
+    int num = i;
+    while (num > 0) {
+        arr[count] = num % 10;
+        num /= 10;
+        count++;
+    }
+    if (isPalindromeRec(arr, 0, count - 1) == 1) {
         return 1;
     }
-    else
-    {
-        
-        return 0;
-    }
-    
+    return 0;
 }
-    
 
-
-
-int isPalindrome(int num)
-{
-    int secNum = subtractingNumber(num, 0);
-
-    if (secNum == num)
-    {
-       
-        return 1 ;
+int isArmstrongRec(int i, int arr[], int index, int end, int total) {
+    if (index == end) {
+        if (total == i) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
+    total = total + ipow(arr[index], end);
+    return isArmstrongRec(i, arr, index + 1, end, total);
+}
 
-    else
-    {
-        return 0;
-    }    
-
+int isArmstrong(int i) {
+    int size = ilog10(i);
+    int arr[size + 1];
+    int count = 0;
+    int num = i;
+    while (num > 0) {
+        arr[count] = num % 10;
+        num /= 10;
+        count++;
+    }
+    if (isArmstrongRec(i, arr, 0, count, 0) == 1) {
+        return 1;
+    }
+    return 0;
 }
